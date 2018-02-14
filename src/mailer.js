d3fdefa0363b6c35145699d772fc47921f26411a
Stasha Blank <stashablank@gmail.com>
2018-02-13 20:54:00 -0500

    Added eslint and circle ci config

--- src/mailer.js
+++ src/mailer.js
@@ -1,26 +1,27 @@
 const helper = require('sendgrid').mail;
-const from_email = new helper.Email('reminder@forget-me-not-sob3966.herokuapp.com');
+
+const fromEmail = new helper.Email('reminder@forget-me-not-sob3966.herokuapp.com');
 const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY || undefined);
 
 const sendEmail = (address, subject, content) => {
-	if(!process.env.SENDGRID_API_KEY){
-		console.log("API Key invalid. Cannot send email.");
-		return;
-	}
-	
-	const to_email = new helper.Email(address);
-	const body = new helper.Content('text/plain', content);
-	const email = new helper.Mail(from_email, subject, to_email, body);
-	
-	const request = sendgrid.emptyRequest({method: 'POST', path: '/v3/mail/send', body: email.toJSON()});
-	
-	sendgrid.API(request, (err, res) => {
-		if(err){
-			console.log(err);
-		}
-	});
+  if (!process.env.SENDGRID_API_KEY) {
+    // console.log('API Key invalid. Cannot send email.');
+    return;
+  }
+
+  const toEmail = new helper.Email(address);
+  const body = new helper.Content('text/plain', content);
+  const email = new helper.Mail(fromEmail, subject, toEmail, body);
+
+  const request = sendgrid.emptyRequest({ method: 'POST', path: '/v3/mail/send', body: email.toJSON() });
+
+  sendgrid.API(request, (err/* , res */) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
 
 module.exports = {
-	sendEmail
+  sendEmail,
 };
--- src/server.js
+++ src/server.js
@@ -5,10 +5,14 @@ const mailer = require('./mailer.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (req, res) => {
-	console.log("Request Received");
-	res.end();
+  // console.log('Request Received');
+  res.end();
 };
 
 http.createServer(onRequest).listen(port);
 
-//mailer.sendEmail("stashablank@gmail.com", "Test Email", "Hi there!");
+if (false) {
+  mailer.sendEmail();
+}
+
+// mailer.sendEmail("stashablank@gmail.com", "Test Email", "Hi there!");
